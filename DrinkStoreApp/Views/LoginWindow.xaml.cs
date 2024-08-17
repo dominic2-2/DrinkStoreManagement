0a8c035ec92096a7d887febf351a2530fd0e0c38
Tran Thanh Binh <binhtthe171542@fpt.edu.vn>
2024-08-18 02:13:33 +0700

    Merge with Hung (Login), add dashboard, user UI

---
+++ DrinkStoreApp/Views/LoginWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DrinkStoreApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DrinkStoreApp.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private readonly DrinkStoreContext _dbContext;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            _dbContext = new DrinkStoreContext();
+        }
+        private bool CheckLogin(string username, string password)
+        {
+            var user = _dbContext.Users
+                .FirstOrDefault(u => u.Username == username && u.Password == password);
+            return user != null;
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUserName.Text.Trim();
+            string password = txtPassword.Password.Trim();
+
+            if (CheckLogin(username, password))
+            {
+                MessageBox.Show("Login successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                var dashboard = new DashboardWindow();
+                dashboard.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+    }
+}
